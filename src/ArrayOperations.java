07e9ca980ffaf66e835ec4f0a1125ebaf19eb941
SOborina <clercler@inbox.ru>
2021-02-19 00:02:52 +0500

    Задача №3_ Массивы

---
+++ src/ArrayOperations.java
@@ -0,0 +1,16 @@
+public class ArrayOperations {
+    public static void main(String[] args) {
+        //Задайте массив из 5 любых целых чисел
+        int[] numbers = {0,1,2,3,4};
+        //Поменяйте местами первый и последний элемент в массиве
+        int lastElement = numbers[numbers.length -1];
+        int firstElement = numbers[0];
+        numbers[numbers.length-1] = firstElement;
+        numbers[0] = lastElement;
+        //Вывести в консоль результат суммы первого и среднего элемента.
+        System.out.println(numbers[0]);
+        System.out.println(numbers[numbers.length-1]);
+        System.out.println("Сумма первого и среднего элемента: " + (numbers[0] + numbers[numbers.length/2]));
+
+    }
+}
